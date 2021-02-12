bc479fbaccac77dc25ddda2077b6a9847ceb105e
ShresthaAsutosh <43807697+ShresthaAsutosh@users.noreply.github.com>
2021-02-12 20:32:02 +1100

    visualising data in the line chart

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { getByRole, queryByTestId, render, screen } from '@testing-library/react';
 import App from './App';
+import Chart from './components/charts/chart';
 
 test('renders learn react link', () => {
   render(<App />);
@@ -10,3 +11,9 @@ test('renders learn react link', () => {
 // Add all the testcase required for the streaming apis
 // Creating context for the dependencies injection
 // possibly directly from the apis testing, if not create a simple mock up object for the test
+
+test('renders the chart', () => {
+
+});
+
+//test if the data is passed from the csv file 
