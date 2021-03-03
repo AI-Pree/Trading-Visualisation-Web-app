7e4bf3b9f1c4e0af984f016920f1ba4c551f9385
ai-pree <sexypistolcs@gmail.com>
2021-03-03 14:13:18 +1100

    removed render from the App function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import Dashboard from './components/dashboard/dashboard';
-import { render } from 'react-dom';
 
 function App() {
-  
-    render(
+
+    return(
         <>
             <Dashboard/>
         </>
-    )
+    );
+
 }
 
 export default App;
